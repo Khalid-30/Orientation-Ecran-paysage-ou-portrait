9fa1c4d6d9f101c8838a75704e9fcb99aabe8f44
Khalid-30 <117428942+Khalid-30@users.noreply.github.com>
2022-11-24 19:04:52 +0100

    DEPOT DES FICHIERS HTML ET JS

---
+++ exo5.js
@@ -0,0 +1,23 @@
+let x= screen.height;
+let y= screen.width;
+let orientation= estEnPaysage(x,y);
+
+function estEnPaysage(x,y){
+
+    if(y>x){
+
+  alert("L'orientation de l'écran est paysage");
+    }
+
+    else {
+  alert("L'orientation de l'écran est portrait");
+    }
+}
+
+
+ 
+
+
+
+
+
